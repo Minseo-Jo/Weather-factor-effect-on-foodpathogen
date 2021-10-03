cb75a7661418ec042283e79813cc2e844cc4b48d
Minseo Jo <87311912+Minseo-Jo@users.noreply.github.com>
2021-10-03 12:59:47 +0900

    Update AWS_bacteria_intoxication logistic.R

--- R data processing/AWS_bacteria_intoxication logistic.R
+++ R data processing/AWS_bacteria_intoxication logistic.R
@@ -1,3 +1,7 @@
+#Original code
+#Copyright (c) 2016, TOBIGS
+#https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=leedk1110&logNo=220774824473
+
 library('arm')
 
 result2 <- bayesglm(bacteria_foodpathogen_intoxication~aLT+aWV+MR
